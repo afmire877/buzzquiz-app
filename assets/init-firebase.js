6e0b05a2c499d562e75552723a68cfd49bc44e9e
Ahmed Mire <afmire877@buzzbar>
2020-06-25 17:10:59 +0100

    Added Game session objects

--- assets/init-firebase.js
+++ assets/init-firebase.js
@@ -14,7 +14,28 @@ const db = firebase.database();
 
 const createRoomFirebase = (id) => {
   db.ref(`/${id}`).set({
-    new: "hell",
+    session_id: id, //  {string } room_id
+    isPlaying: false, // {boolean}
+    totalRounds: 0, // {number}
+    currentQIndex: 0,
+    player: [
+      {
+        name: "Player 1",
+        avatar_uri: "https://avatarfiles.alphacoders.com/165/165274.png",
+        totalPoints: 0,
+      },
+      {
+        name: "Player 2",
+        avatar_uri: "https://avatarfiles.alphacoders.com/165/165274.png",
+        totalPoints: 0,
+      },
+      {
+        name: "Player 3",
+        avatar_uri: "https://avatarfiles.alphacoders.com/165/162274.png",
+        totalPoints: 0,
+      },
+    ],
+    quizData: 1,
   });
 };
 // update state in Firebase
@@ -23,3 +44,17 @@ const updateRoomFirebase = () => {};
 const removeRoomFromFirebase = (id) => {
   db.ref(`/${id}`).remove();
 };
+
+// quizData = [
+//   [
+//     {
+//       id: "", // {string} for ref
+//       type: "", // {string} Avalible types: "Image", "text", "match"
+//       question: "", //  {string}
+//       correct: "", // {string}
+//       hasTimer: true, //  {boolean}
+//       timer: 10, // {number} in seconds
+//       points: null, // Number
+//     },
+//   ],
+// ]
